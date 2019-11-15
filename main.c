a7565913b3486e3fb2a286bd37e5ba18c30702cc
mmiah00 <mmiah00@stuy.edu>
2019-11-14 22:14:29 -0500

    added check

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 int main () {
         struct stat s;
         int f = stat ("Makefile", &s);
+	if (f < 0) { 
+		printf ("Error %d: %s\n", errno, strerror (errno)); 
+	}
         printf ("File: Makefile\n\tSize: %d\n", f.st_size);
         printf ("Mode: %o\n", f.st_mode); //in octal numbers
         time_t tim = f.st_atime;
