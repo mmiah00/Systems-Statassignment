b1855866a7a25877c27878822e43e590c2308ca8
mmiah00 <mmiah00@stuy.edu>
2019-11-14 22:09:58 -0500

    main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys.stat.h>
+#include <time.h>
+#include <string.h>
+
+int main () {
+        struct stat s;
+        int f = stat ("Makefile", &s);
+        printf ("File: Makefile\n\tSize: %d\n", f.st_size);
+        printf ("Mode: %o\n", f.st_mode); //in octal numbers
+        time_t tim = f.st_atime;
+        printf ("Last accessed: %s", ctime (&f.st_atime));
+  return 0;
+}
+
